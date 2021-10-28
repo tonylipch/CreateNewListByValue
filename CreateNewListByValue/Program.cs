93716287db9079563a99a5acfba86e7b09068e27
tonylipch <tony.lipchansky@gmail.com>
2021-10-28 21:00:46 +0300

    aaa

---
+++ CreateNewListByValue/Program.cs
@@ -0,0 +1,130 @@
+﻿using NPOI.SS.Formula.Functions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+//
+
+namespace CreateNewListByValue {
+
+    
+
+
+   class FindKey {
+
+        public void newList(List<T> originList , List<int> afterKey ,List<int> beforeKey, int key) 
+        {
+
+ 
+            int i = 0;
+
+            originList.Add(2,4,5) ;
+   
+            for (; i <originList.Count; )
+            {
+
+              
+
+
+                if (key.Equals(newNote.key))
+                {
+                    break;
+
+                }
+
+
+            }
+
+            for (; i<originList.Count;)
+            {
+                afterKey.AddRange(originList);
+
+
+                if (key.Equals(newNote.key))
+                {
+                    break;
+
+                }
+
+
+            }
+
+            for (; i < afterKey.Count;)
+            {
+                beforeKey.AddRange(afterKey);
+
+
+               
+
+
+            }
+
+
+
+        
+
+
+
+
+    
+
+
+
+
+        }
+
+    }
+
+
+
+
+    public class Example
+    {
+        public static void Main()
+        {
+         /*   // Create a list of parts.
+            List<CreateList> list = new List<CreateList>();
+
+            // Add parts to the list.
+
+            list.Add(new CreateList {key= 1,value="Baba" });
+            list.Add(new CreateList { key = 2, value = "Mama" });
+            list.Add(new CreateList { key = 3, value = "Papa" });
+            list.Add(new CreateList { key = 4, value = "Kaka" });
+            list.Add(new CreateList { key = 5, value = "Taka" });
+
+          
+
+
+
+            // Write out the parts in the list. This will call the overridden ToString method
+            // in the Part class.
+            Console.WriteLine();
+            foreach (CreateList li in list)
+            {
+                Console.WriteLine(li);
+
+            }
+
+            Console.WriteLine();
+
+
+
+
+            
+
+   
+
+*/
+
+
+
+
+
+
+
+
+
+        }
+    }
