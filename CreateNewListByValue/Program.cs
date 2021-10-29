ed0084251492bb0b90f5142acbd8cd67c71e2bd3
Anton Lipchansky <tony.lipchansky@gmail.com>
2021-10-29 12:26:14 +0300

    final result

--- CreateNewListByValue/Program.cs
+++ CreateNewListByValue/Program.cs
@@ -4,145 +4,71 @@ using System.Collections.Generic;
 using System.Linq;
 
 
-//
 
-namespace CreateNewListByValue {
-
-
-    public class List 
-    {
-  
-        public int value{ get; set; }
-
-        public override string ToString()
-        {
-            return "value: "+value ;
-        }
-     
-    }
-
-
-
-
-    class Findvalue {
-
-        public void newList(List<List> originList , List<List> aftervalue ,List<List> beforevalue, int value) 
-        {
-            List list = new List();
-
-            list.value = value;
-
-
- 
-            int i = 0;
-
-
-            originList.Add(list);
-         
-            for (; i <originList.Count; )
-            {
-
-
-                originList.AddRange(aftervalue);
-
-                if (list.value==value)
-                {
-                    break;
-
-                }
-
-
-            }
-
-            for (; i<originList.Count;)
-            {
-                aftervalue.AddRange(originList);
-
-
-                if (value == list.value)
-                {
-                    break;
-
-                }
-
-
-            }
-
-            for (; i < aftervalue.Count;)
-            {
-                beforevalue.AddRange(aftervalue);
-
-
-               
-
-
-            }
-
-
-
-        
-
-
-
-
-    
-
-
-
-
-        }
-
-    }
-
-
-
+namespace CreateNewListByValue
+{
 
     public class Example
     {
-        public static void Main()
+
+        private static void printList(List<int> list)
         {
-         /*   // Create a list of parts.
-            List<CreateList> list = new List<CreateList>();
 
-            // Add parts to the list.
-
-            list.Add(new CreateList {value= 1,value="Baba" });
-            list.Add(new CreateList { value = 2, value = "Mama" });
-            list.Add(new CreateList { value = 3, value = "Papa" });
-            list.Add(new CreateList { value = 4, value = "Kaka" });
-            list.Add(new CreateList { value = 5, value = "Taka" });
-
-          
-
-
-
-            // Write out the parts in the list. This will call the overridden ToString method
-            // in the Part class.
-            Console.WriteLine();
-            foreach (CreateList li in list)
+            string sep = "";
+            foreach (var x in list)
             {
-                Console.WriteLine(li);
-
+                Console.Write(sep);
+                Console.Write(x);
+                sep = ", ";
             }
-
             Console.WriteLine();
-
-
-
-
-            
-
-   
-
-*/
-
-
-
-
-
-
-
 
 
         }
+
+        public static void splitListByKey(List<int> origin, List<int> beforevalue, List<int> aftervalue, int key)
+        {
+
+            int i = 0;
+            for (; i < origin.Count; i++)
+            {
+
+                beforevalue.Add(origin[i]);
+                if (origin[i] == key)
+                {
+                    break;
+                }
+
+            }
+            i++;
+            for (; i < origin.Count; i++)
+            {
+                aftervalue.Add(origin[i]);
+            }
+        }
+
+        public static void Main()
+        {
+
+            List<int> origin = new List<int>();
+            origin.Add(1);
+            origin.Add(2);
+            origin.Add(3);
+            origin.Add(4);
+            origin.Add(5);
+            origin.Add(6);
+            origin.Add(7);
+
+            List<int> before = new List<int>();
+            List<int> after = new List<int>();
+
+            splitListByKey(origin, before, after, 4);
+            printList(origin);
+            printList(before);
+            printList(after);
+
+            Console.ReadKey();
+
+        }
     }
+}
